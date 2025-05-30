5dd945638a7611f85314a35758d010eabb895524
coderboi33 <132617957+coderboi33@users.noreply.github.com>
2025-05-31 05:15:54 +0530

    v1 DEPLOYMENT

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -6,7 +6,7 @@ import { api } from "../../../../convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-    secret: "sk_dev_O0o6bXRru3LNaWz3QYnkUkYcuBduHmV0IiHeV9pOTXxQzdhhXmWLk6y5-GeAWJ8V",
+    secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(req: Request) {
