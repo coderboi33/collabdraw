7f8ada7a602f8f97e126995ed6060c12ae8d210e
coderboi33 <132617957+coderboi33@users.noreply.github.com>
2025-05-31 05:44:52 +0530

    minor change 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import {
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Providers from '../../providers/Providers';
+import { Suspense } from 'react';
+import Loading from '@/components/Loading';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,9 +32,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <Providers>
-            {children}
-          </Providers>
+          <Suspense fallback={<Loading />}>
+            <Providers>
+              {children}
+            </Providers>
+          </Suspense>
         </body>
       </html>
     </ClerkProvider>
